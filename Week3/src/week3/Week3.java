82103f408bac233ce6913fce69f4bb8b7bab3dce
adamhawkins99 <32527229+adamhawkins99@users.noreply.github.com>
2019-01-24 06:41:39 -0800

    Update Week3.java

--- Week3/src/week3/Week3.java
+++ Week3/src/week3/Week3.java
@@ -1,7 +1,5 @@
-/*Testing link to github from netbeans
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*Breif summary of what I learned this week.  
+
  */
 package week3;
 
@@ -12,10 +10,13 @@ package week3;
 public class Week3 {
 
     /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+     *Threading is a huge part of Java.  It allows or multiple operations to run both synchronous and Asynchronous
+     *It does this in two ways, either a Thread class or Runnable interface.  In the examples this week I show both then also show
+     *an executor example.  The executor is an interface for launching a new task.  You then need handlers to schedule runnables.  
+     *I also used a Try and Catch block in order to handle any exceptions.  
+     *Thanks Brother Tuckett, I am starting to begin to understand how this all comes together.  
+     
+   \
     }
     
 }
